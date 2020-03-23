226a0cf7ebe847d8aa613e0e2b36b9493839d516
lokesh_d <lokesh@tralamo.in>
2020-03-23 13:36:38 +0530

    2nd commit

--- python/unpackingtuple.py
+++ python/unpackingtuple.py
@@ -2,3 +2,9 @@ l=[(1,4),(45,54),(58,77),(77,99),(10,12)]
 for a,b in l:
     print(a)
     print(b)
+l1=["loki","venu","navneeth","saqi"]
+l2=[10,11,12,13]
+s=zip(l1,l2)
+print(list(s))
+for a,b in s:
+    print('{}      ---{}'.format(a,b))
