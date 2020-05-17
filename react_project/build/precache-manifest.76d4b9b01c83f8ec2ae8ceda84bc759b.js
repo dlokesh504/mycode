04cdd34ad7b10ba7868f0cd620be2f6262f8f419
lokesh_d <lokesh@tralamo.in>
2020-05-17 20:32:28 +0530

    react added

---
+++ react_project/build/precache-manifest.76d4b9b01c83f8ec2ae8ceda84bc759b.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536684000a88f0f79eda060d81a39620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "948c0df671c473349981",
+    "url": "/static/css/2.423a5943.chunk.css"
+  },
+  {
+    "revision": "4276d1647bfd155da7c2",
+    "url": "/static/css/main.4d4b237d.chunk.css"
+  },
+  {
+    "revision": "948c0df671c473349981",
+    "url": "/static/js/2.16e3ea4f.chunk.js"
+  },
+  {
+    "revision": "8c4dd856e532d7f6c76ae9de2c87e0bb",
+    "url": "/static/js/2.16e3ea4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4276d1647bfd155da7c2",
+    "url": "/static/js/main.561a1fde.chunk.js"
+  },
+  {
+    "revision": "6cfed5d07469bac4a678",
+    "url": "/static/js/runtime-main.897a52c7.js"
+  },
+  {
+    "revision": "270f1e08c028a22f432af727462c6fdc",
+    "url": "/static/media/1.270f1e08.webp"
+  },
+  {
+    "revision": "4fa2c2a28e39a75a653aba7899d7d20f",
+    "url": "/static/media/2a.4fa2c2a2.svg"
+  },
+  {
+    "revision": "c2c0948b5cd6c8332c8cb2fac50e6d1c",
+    "url": "/static/media/4a.c2c0948b.webp"
+  },
+  {
+    "revision": "d13a114aa55cc1583b112232d490484f",
+    "url": "/static/media/a.d13a114a.jpg"
+  },
+  {
+    "revision": "61d9846367dcc835b1dc01b9526ccc2c",
+    "url": "/static/media/b.61d98463.jpg"
+  },
+  {
+    "revision": "4bffa68e08528568336b2c8fa8fe077f",
+    "url": "/static/media/bank.4bffa68e.jpg"
+  },
+  {
+    "revision": "d19abc001bd2996b140062b8c8803c5a",
+    "url": "/static/media/cara.d19abc00.png"
+  },
+  {
+    "revision": "0a7f32b806d144a7e477dadd2bb0b754",
+    "url": "/static/media/cogh.0a7f32b8.jpg"
+  },
+  {
+    "revision": "6d6bc2ccfe290010b612aaefddfd460a",
+    "url": "/static/media/corona.6d6bc2cc.jpg"
+  },
+  {
+    "revision": "5a476393c86aa3ade62a63600021b7cb",
+    "url": "/static/media/d42a4e22ee344ff5a9f5061d4869cee1_18.5a476393.jpg"
+  },
+  {
+    "revision": "236e9c9391adf308eafd7c86b7015301",
+    "url": "/static/media/download.236e9c93.jpeg"
+  },
+  {
+    "revision": "2fb9add035921fdf448bd73b3d04f3c4",
+    "url": "/static/media/fr.2fb9add0.jpg"
+  },
+  {
+    "revision": "3acfbe477bfdf31b68d83fc013deacb0",
+    "url": "/static/media/fruit.3acfbe47.jpeg"
+  },
+  {
+    "revision": "d4fcf5cc3a5e768503eaccb7ff6e9859",
+    "url": "/static/media/grocery.d4fcf5cc.jpg"
+  },
+  {
+    "revision": "2e40ff3f4cb26dfc2d2faaab51368ea5",
+    "url": "/static/media/id verify.2e40ff3f.png"
+  },
+  {
+    "revision": "a1d49eb16987dff6bde4be0b033b309a",
+    "url": "/static/media/id.a1d49eb1.jpg"
+  },
+  {
+    "revision": "d19eb960e5ea43dcfcfb50b80b80c0ab",
+    "url": "/static/media/insta.d19eb960.png"
+  },
+  {
+    "revision": "8a4053ac585059669df2949d165471f6",
+    "url": "/static/media/loglog.8a4053ac.png"
+  },
+  {
+    "revision": "bbee6353318e4628c940b0472ca75aa4",
+    "url": "/static/media/loki.bbee6353.jpg"
+  },
+  {
+    "revision": "3e4087130dffc1cc3d5bed7b428be90d",
+    "url": "/static/media/loki1.3e408713.jpg"
+  },
+  {
+    "revision": "61c9addb7f855972ebb597b85e433e21",
+    "url": "/static/media/med.61c9addb.jpeg"
+  },
+  {
+    "revision": "0cddab0f2ab6b46671bb6365c32e20e1",
+    "url": "/static/media/ram.0cddab0f.jpeg"
+  },
+  {
+    "revision": "9f467ba75e887155e322f038d0d7af44",
+    "url": "/static/media/reg.9f467ba7.png"
+  },
+  {
+    "revision": "dbacd3aa4e98161eaf36b73b09abd500",
+    "url": "/static/media/sam.dbacd3aa.png"
+  },
+  {
+    "revision": "36f8e30485f959765170da3aa0e02d89",
+    "url": "/static/media/sam1.36f8e304.jpg"
+  },
+  {
+    "revision": "235417fb2d3556878968302485302478",
+    "url": "/static/media/samsam.235417fb.jpg"
+  },
+  {
+    "revision": "ac51905c8862e703c4534b8587c21ecd",
+    "url": "/static/media/sd.ac51905c.png"
+  },
+  {
+    "revision": "b4f9f97ef17f943e969494a3f8e57cfb",
+    "url": "/static/media/shop.b4f9f97e.jpg"
+  },
+  {
+    "revision": "d600327f1ad9cebed7445b1fe77c116e",
+    "url": "/static/media/social.d600327f.jpg"
+  },
+  {
+    "revision": "1f05056851b1e7afad89195dff636acf",
+    "url": "/static/media/socialdistancingishanistorypic_1584528935_684x384.1f050568.jpeg"
+  },
+  {
+    "revision": "1a3e7a6992c8cfa0a46f5e2361a3e494",
+    "url": "/static/media/vCita-Appointment-Scheduling-and-Online-Booking-Plugin-Featured-Image.1a3e7a69.png"
+  },
+  {
+    "revision": "25837d1307790fa407fc2d01da120d45",
+    "url": "/static/media/verify.25837d13.png"
+  },
+  {
+    "revision": "2d33570e31c0db1ddf12e71ec813a851",
+    "url": "/static/media/verify.2d33570e.png"
+  }
+]);
